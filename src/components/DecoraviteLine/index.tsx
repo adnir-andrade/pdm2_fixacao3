330639bcf6b651f78c8d3fdeccf3870ee0a2fce4
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-20 20:58:16 -0300

    Implement DecorativeLine component

---
+++ src/components/DecoraviteLine/index.tsx
@@ -0,0 +1,12 @@
+import { Image } from 'react-native';
+import React from 'react';
+
+export default function DecorativeLine() {
+  return (
+    <Image
+      source={require('../../../assets/images/Decoration.png')}
+      className='absolute top-0 w-[115%] h-full'
+      resizeMode='contain'
+    />
+  );
+}
