f22b40f39e1ccc32b962098d0834e7b4961f5a7e
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-20 23:16:10 -0300

    Add components to main screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native';
+import { FlatList, View } from 'react-native';
+import Text from '../components/Text';
 import React from 'react';
+import Card from '../components/Card';
+import users from '../mocks/MOCK_DATA';
 
 export default function Index() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <>
+      <View className='pt-36 bg-slate-900'>
+        <View className='mb-12'>
+          <Card.Header>
+            <Text.Title>Username</Text.Title>
+          </Card.Header>
+        </View>
+        <Card.Body>
+          <FlatList
+            data={users}
+            renderItem={({ item }) => <Text>{item.username}</Text>}
+            keyExtractor={(user) => user.id.toString()}
+          ></FlatList>
+        </Card.Body>
+        <Card.Footer>
+          <Text>Developed by Adnir Andrade</Text>
+        </Card.Footer>
+      </View>
+    </>
   );
 }
