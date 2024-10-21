ec2e6ede20b6f00741e71288c60e6e1781bf126c
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-21 03:43:05 -0300

    Remove unused imports

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,6 @@
-import { FlatList, View } from 'react-native';
-import Text from '../components/Text';
+import { View } from 'react-native';
 import React, { useState } from 'react';
 import Card from '../components/Card';
-import users from '../mocks/MOCK_DATA';
-import Divider from '../components/Divider';
-import PressableItem from '../components/PressableItem';
 import HeaderLabel from '../components/HeaderLabel';
 import Footer from '../components/Footer';
 import UserList from '../components/UserList';
