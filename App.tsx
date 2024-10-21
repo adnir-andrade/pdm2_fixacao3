66f5826a4ffce2ac9ed91e9b51121f5642318508
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-20 21:49:52 -0300

    Configure and initialize fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,48 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Index from './src/app';
+import {
+  useFonts,
+  Cinzel_400Regular,
+  Cinzel_500Medium,
+  Cinzel_600SemiBold,
+  Cinzel_700Bold,
+  Cinzel_800ExtraBold,
+  Cinzel_900Black,
+} from '@expo-google-fonts/cinzel';
+import {
+  CinzelDecorative_400Regular,
+  CinzelDecorative_700Bold,
+  CinzelDecorative_900Black,
+} from '@expo-google-fonts/cinzel-decorative';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Cinzel_400Regular,
+    Cinzel_500Medium,
+    Cinzel_600SemiBold,
+    Cinzel_700Bold,
+    Cinzel_800ExtraBold,
+    Cinzel_900Black,
+    CinzelDecorative_400Regular,
+    CinzelDecorative_700Bold,
+    CinzelDecorative_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
