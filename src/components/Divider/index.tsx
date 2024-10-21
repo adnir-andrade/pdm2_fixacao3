197aee2e5100cbda6a09d5d69c1c0bc95ae5eb12
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-21 02:29:13 -0300

    Implement Divider component

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function Divider() {
+  return (
+    <View className='w-full self-stretch shadow border border-themys-straw/20 blur-md'></View>
+  );
+}
