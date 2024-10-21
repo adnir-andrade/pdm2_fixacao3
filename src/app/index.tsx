f416139e1cac423df76fe79e02c6f266a3711cc3
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-21 02:42:16 -0300

    Implement event callback to change title

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,34 @@
 import { FlatList, View } from 'react-native';
 import Text from '../components/Text';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 import users from '../mocks/MOCK_DATA';
+import Divider from '../components/Divider';
+import PressableItem from '../components/PressableItem';
 
 export default function Index() {
+  const [activeUser, setActiveUser] = useState('Select User');
+
   return (
     <>
       <View className='pt-36 bg-slate-900'>
         <View className='mb-12'>
           <Card.Header>
-            <Text.Title>Username</Text.Title>
+            <Text.Title>{activeUser}</Text.Title>
           </Card.Header>
         </View>
         <Card.Body>
           <FlatList
             data={users}
-            renderItem={({ item }) => <Text>{item.username}</Text>}
+            renderItem={({ item }) => (
+              <View className='flex flex-col space-y-4'>
+                <PressableItem
+                  text={item.username}
+                  onPress={() => setActiveUser(item.name)}
+                />
+                <Divider />
+              </View>
+            )}
             keyExtractor={(user) => user.id.toString()}
           ></FlatList>
         </Card.Body>
