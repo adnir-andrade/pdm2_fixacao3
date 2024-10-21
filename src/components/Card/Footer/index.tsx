6dc14571c60c7b797bc468e83b67a31355202522
Adnir Andrade <adnir_andrade@hotmail.com>
2024-10-21 03:06:37 -0300

    Adjust Footer positioning once more

--- src/components/Card/Footer/index.tsx
+++ src/components/Card/Footer/index.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 
 export default function Card({ children }: CardProps) {
   return (
-    <View className='w-full h-12 bg-themys-tussock rounded-lg border border-themys-cod-gray/60 items-center'>
+    <View className='w-full h-12 bg-themys-tussock rounded-lg border border-themys-cod-gray/60 items-center justify-center bottom-0 absolute'>
       <>{children}</>
     </View>
   );
